a5685efce46c7704d73fbaf214763d36e3fcba2c
ChristianMedi <christianmed14@outlook.com>
2022-12-22 19:04:04 -0500

    Commit Messsage

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Contact = () =>{
-    return <h1>Contact</h1>
+    return <><h1>Contact</h1>
+    <h3>Heres a list of my contact info</h3></>
 }
 export default Contact;
